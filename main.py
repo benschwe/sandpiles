dd524ffda835d943cceffa05dba92071d6073e34
benschwe <benschwe@gmail.com>
2023-06-04 21:06:20 -0700

    Minor changes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #%%
-import sandpileClass
+import sandpile
 import numpy as np
 from matplotlib import pyplot as plt
 import time
@@ -7,43 +7,48 @@ import time
 #%%
 # Set up piles
 sandpiles = {}
-simSteps = 30000
-sandpiles['small'] = sandpileClass.sandpile(15, simSteps)
-sandpiles['med'] = sandpileClass.sandpile(40, simSteps)
+simSteps = 20000
+sandpiles['small'] = sandpile.sandpile(30, simSteps)
+#sandpiles['med'] = sandpileClass.sandpile(20, simSteps)
 
 #%%
 # Main loop
 
 for size in sandpiles.keys():
+    
     step = 0
     print('Working on pile: ' + str(size))
     
     while step < sandpiles[size].simSteps:
         
-        sandpiles[size].addSand(1, int(sandpiles[size].numEdgePoints/2), 
-                                   int(sandpiles[size].numEdgePoints/2))
+        sandpiles[size].addSand(1, int(sandpiles[size].numEdgePoints / 2), 
+                                   int(sandpiles[size].numEdgePoints / 2))
     
         start = time.time()
+        
         if sandpiles[size].anyHigherThanThreshold(): 
             
             avalancheLength = 0
             
             while sandpiles[size].anyHigherThanThreshold():
+                
                 row, col = sandpiles[size].findHighestPoint()
                 sandpiles[size].redistributeSand(row, col)
                 sandpiles[size].clearEdges()
                 avalancheLength += 1
                 
             sandpiles[size].avalancheHistory[step] = avalancheLength
+        
         end = time.time()
         
         sandpiles[size].avalancheTime[step] = end - start
         step += 1  
         
         if step % 500 == 0:
-            print ('{:.0f} percent complete'.format(100.0*(step/float(sandpiles[size].simSteps))))
+            
+            print ('{:.0f} percent complete'.format(100 * (step / float(sandpiles[size].simSteps))))
             print ('Elapsed time for last 500 steps: {:.2f}'.format(np.sum(sandpiles[size].avalancheTime
-                                                               [step - 500:step])))
+                                                                    [step - 500:step])))
             
     sandpiles[size].hist = (np.histogram(sandpiles[size].avalancheHistory
                             [sandpiles[size].avalancheHistory > 0], 50))
@@ -51,6 +56,7 @@ for size in sandpiles.keys():
 #%%
 # Plotting
 for size in sandpiles.keys():
+    
     plt.figure(1)
     plt.loglog(sandpiles[size].hist[0], sandpiles[size].hist[1][1:], 
                linestyle = 'None', marker = '.', markersize = 10, label = size)
@@ -58,8 +64,11 @@ for size in sandpiles.keys():
     plt.xlabel('Size of avalanche')
     plt.ylabel('# of avalanches')
     plt.legend()
+    plt.show()
     
     plt.figure(2)
     plt.plot(np.arange(0,sandpiles[size].simSteps, 1), 
              sandpiles[size].avalancheHistory, label = size)
     plt.legend()
+    plt.show()
+# %%
---
+++ sandpile.py
@@ -0,0 +1,54 @@
+import numpy as np 
+
+class sandpile:
+    
+#***Threshold hardcoded as 4***
+    
+    def __init__(self, numEdgePoints=30, simSteps=1000):
+        
+        self.numEdgePoints = numEdgePoints
+        self.sandpileArray = np.zeros((self.numEdgePoints, self.numEdgePoints))
+        self.avalancheHistory = np.zeros(simSteps)
+        self.avalancheTime = np.zeros(simSteps)
+        self.simSteps = simSteps
+        
+
+    def addSand(self, sandQty, locX, locY):
+        
+        self.sandpileArray[locX, locY] += sandQty
+    
+    
+    def anyHigherThanThreshold(self):
+        
+        return (np.any(self.sandpileArray > 4))
+        
+            
+    def findHighestPoint(self):
+        
+        row = int((np.argmax(self.sandpileArray) / np.size(self.sandpileArray, 0)))
+        col = np.argmax(self.sandpileArray) % np.size(self.sandpileArray, 0)
+        
+        return row, col
+    
+    
+    def redistributeSand(self, row, col):
+        
+        if self.sandpileArray[row, col] > 4:
+            
+            self.sandpileArray[row, col] -= 4
+            self.sandpileArray[row + 1, col] += 1
+            self.sandpileArray[row - 1, col] += 1
+            self.sandpileArray[row, col + 1] += 1
+            self.sandpileArray[row, col - 1] += 1
+        
+    
+    def clearEdges(self):
+        
+        self.sandpileArray[:, 0] = 0
+        self.sandpileArray[:, -1] = 0
+        self.sandpileArray[0, :] = 0
+        self.sandpileArray[-1, :] = 0
+        
+        
+    
+        
